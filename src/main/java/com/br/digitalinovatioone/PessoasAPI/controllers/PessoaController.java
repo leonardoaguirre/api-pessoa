c513f4fb074346b2f6ecf15da72d700368b5ee27
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2021-12-12 19:07:51 -0300

    Adiciona end point para cadastro de pessoa(postmapping)

--- src/main/java/com/br/digitalinovatioone/PessoasAPI/controllers/PessoaController.java
+++ src/main/java/com/br/digitalinovatioone/PessoasAPI/controllers/PessoaController.java
@@ -1,15 +1,32 @@
 package com.br.digitalinovatioone.PessoasAPI.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.br.digitalinovatioone.PessoasAPI.dto.ResponseMessage;
+import com.br.digitalinovatioone.PessoasAPI.entities.Pessoa;
+import com.br.digitalinovatioone.PessoasAPI.repositories.PessoaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/pessoa")
 public class PessoaController {
+    private final PessoaRepositorio pessoaReporitorio;
+
+    @Autowired
+    public PessoaController(PessoaRepositorio pessoaReporitorio) {
+        this.pessoaReporitorio = pessoaReporitorio;
+    }
 
     @GetMapping
     public String getPessoa() {
-        return "Olá esta é minha API";
+        return "Olá esta é minha api";
+    }
+
+    @PostMapping
+    public ResponseMessage criaPessoa(@RequestBody Pessoa pessoa) {
+        Pessoa pessoaSaved = pessoaReporitorio.save(pessoa);
+        return ResponseMessage
+                .builder()
+                .message("Pessoa criada com sucesso! ID: " + pessoaSaved.getId())
+                .build();
     }
 }
--- src/main/java/com/br/digitalinovatioone/PessoasAPI/repositories/PessoaRepositorio.java
+++ src/main/java/com/br/digitalinovatioone/PessoasAPI/repositories/PessoaRepositorio.java
@@ -3,5 +3,5 @@ package com.br.digitalinovatioone.PessoasAPI.repositories;
 import com.br.digitalinovatioone.PessoasAPI.entities.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public abstract class PessoaReporitorio implements JpaRepository<Pessoa, Long> {
+public abstract class PessoaRepositorio implements JpaRepository<Pessoa, Long> {
 }
